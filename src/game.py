fed91d76f9124115fab2f6cc05e023beb98bd702
Lele1409 <leander.decoene@gmail.com>
2023-06-30 17:31:33 +0200

    adapt some parts to be in line with the rules

--- src/game.py
+++ src/game.py
@@ -1,4 +1,8 @@
 import random as rd
+import time
+
+# TODO: add a toggleable print function for more information when 'human' playing
+MODE = 'random'
 
 
 class Game:  # TODO: in a 3 player game, cards are dealt four at a time
@@ -8,10 +12,15 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         4: 6,
         5: 3
     }
+    # Information about the number of cards dealt at once to a player
+    dealSizes = {
+        3: 4,
+        4: 3,
+        5: 3
+    }
     # All the possible contracts
     contracts = ['pass', 'small', 'guard', 'guard w/o', 'guard against']
 
-    # noinspection SpellCheckingInspection
     def __init__(self, players=4, deck=None, matchPoints=None, lastDealer=None):
         # VALIDATE PARAMETERS
         # PARAM PLAYERS: is limited to specific values as described by the following error message:
@@ -20,7 +29,7 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         if type(players) is int:
             if players not in [3, 4, 5]:
                 raise GameException(paramPlayersExceptionString)
-            self.players = [Player(name=f'Player {n}', strategy='human') for n in range(players)]
+            self.players = [Player(name=f'Player {n}', strategy=MODE) for n in range(players)]
 
         # If the Players already existed
         elif type(players) is list:
@@ -43,9 +52,13 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
 
         # PARAM DECK: can be set from previous game, or a new deck will be created and shuffled
         if deck is None:
-            deck = self._create_deck()
+            deck = self.create_deck()
             rd.shuffle(deck)
-        self.deck = deck
+        if type(deck) is list and sorted(deck) == sorted(self.create_deck()):
+            self.deck = deck
+        else:
+            raise GameException(f"""Parameter deck has to be a list of strings, where the strings have to be the same 
+                                 as in a newly generated deck""")
 
         # PARAM LASTDEALER: can be set from previous game, making the dealer change by one player
         if lastDealer is None:
@@ -54,7 +67,7 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
             self.dealer = lastDealer
             self._nextDealer()
         else:
-            raise GameException("Parameter lastDealer has to be None or a integer in range()")
+            raise GameException(f"Parameter lastDealer has to be None or a integer in range(0, {self.playerCount + 1})")
 
         # INITIALIZE VARIABLES
         # A list containing the cards in the dog
@@ -65,7 +78,8 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         self.highestContract = 'pass'
         self.playerTaking = None
 
-    def _create_deck(self) -> list:
+    @staticmethod
+    def create_deck() -> list:
         """Return a list of the 78 cards: four suits, all trumps and the excuse"""
         suits = ['♤', '♡', '♧', '♢']
         values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'C', 'Q', 'K']
@@ -79,7 +93,7 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         deck = cards + trumps + ['EX']
         return deck
 
-    def start(self):
+    def play(self):
         # Make the players choose a contract after the cards are dealt to them
         # If no contract is chosen, start over by re-dealing
         while self.highestContract == 'pass':
@@ -87,9 +101,12 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
             self._awaitContracts()
 
         # Give the dog to the player that is taking or to the defendants
-        self._dogToAside()
+        self._convertDogToAside()
 
-        # play tricks (param trick:int)
+        # Give each player the opportunity to announce a Chelem
+        # TODO: Chelem
+
+        # Play tricks (param trick:int)
         print('PLAYING FIRST TRICK')
 
     def _deal(self):  # TODO: add start deal at right of dealer
@@ -98,11 +115,12 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
 
         # The deck gets cut at some random place, but not closer to the ends of the deck than the size of a deal
         # to a player
-        deckCutIndex = rd.randint(0 + 3, len(self.deck) - 1 - 3)
+        deckCutIndex = rd.randint(0 + Game.dealSizes[self.playerCount],
+                                  len(self.deck) - 1 - Game.dealSizes[self.playerCount])
         self.deck = self.deck[deckCutIndex:] + self.deck[:deckCutIndex]
 
         # List of booleans specifying the deal type
-        deals = self._specifyDealOrder()
+        deals = self._getDealOrder()
 
         nextPlayer = 0
         for deal in deals:
@@ -111,15 +129,23 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
                 self._dog.append(self.deck.pop(0))
             else:
                 # Give the three first cards of the deck to the next player
-                cardsDealt = [self.deck.pop(0) for _ in range(3)]
-                self.players[nextPlayer].addCardsToHand(cardsDealt)
+                cardsDealt = [self.deck.pop(0) for _ in range(Game.dealSizes[self.playerCount])]
+                self.players[nextPlayer].addCardsToHand(cardsDealt, sort=False)
 
                 # Set the nextPlayer to what would be the player to the right of the current player
                 nextPlayer -= 1
                 if nextPlayer < 0:
                     nextPlayer = self.playerCount - 1
 
-    def _specifyDealOrder(self) -> list:
+        # Repeat this function recursively until no player gets a hand containing: only the first trump ('1T') and not
+        # the excuse ('EX')
+        for player in self.players:
+            if '1T' in player.hand and 'EX' not in player.hand:
+                if len([card for card in player.hand if 'T' in card]) == 1:
+                    self._resetDeck()
+                    self._deal()
+
+    def _getDealOrder(self) -> list:
         """Depending on the playerCount (which may result in different dog sizes) create a list of booleans with a
         number of Trues equal to the dogSize"""
 
@@ -128,7 +154,7 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         # 78 is the total number of cards and 3 the number of cards given to a player at once, in an IRL game the
         # nbOfDeals would represent the number of times a dealer has to take cards from the deck and put them somewhere
         # else, either in a player hand or into the dog. In this function, it represents the length of the deal list.
-        nbOfDeals = int((78 - dogSize) / 3 + dogSize)
+        nbOfDeals = int((78 - dogSize) / Game.dealSizes[self.playerCount] + dogSize)
         deals = [False for _ in range(nbOfDeals)]
 
         # Loop in which some deals become "dog-deals" meaning that the cards go into the dog and not into a player's
@@ -162,9 +188,9 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
 
         # Re-deal if no one chose a contract
         if self.highestContract == 'pass':
-            self.resetDeck()
+            self._resetDeck()
 
-    def resetDeck(self):
+    def _resetDeck(self):
         # For every player
         for i in range(self.playerCount):
             # Reset the player's hands and get back the cards in the following order:
@@ -174,26 +200,34 @@ class Game:  # TODO: in a 3 player game, cards are dealt four at a time
         self.deck.extend(self._dog)
         self._dog = []
 
-    def _dogToAside(self) -> None:
+    def _convertDogToAside(self) -> None:
         """Transfer the cards of the dog to whatever player should get it"""
+        playerTaking = self.players[self.playerTaking]
+
         # If the taker takes with a 'small' or a 'guard' contract, he gets the cards into his hand and has to put back
         # the same number of cards into his aside (same as Player.cardsWon since the cards are part of the players final
         # points)
         if self.highestContract in Game.contracts[1:3]:
-            self.players[self.playerTaking].addCardsToHand(self._dog)
+            playerTaking.addCardsToHand(self._dog)
 
-            # If the cards are shown to the other players (not the case if 'guard w/o' or 'guard against')
+            # The cards are shown to the other players (not the case if 'guard w/o' or 'guard against')
             self.dog = self._dog
             self._dog = []
 
             for _ in range(Game.dogSizes[self.playerCount]):
-                self.players[self.playerTaking].putCardToAside()
+                playerTaking.putCardInAside()
 
-        #
+        # In case the contract is a 'guard w/o' the player taking takes the card from the dog without looking at
+        # them
         elif self.highestContract == Game.contracts[3]:
-            pass
+            playerTaking.takeCardsWon(self._dog)
 
-    def end(self) -> tuple:  # TODO: export data for other game
+        # In case the contract is a 'guard against' the points of the dog are given to the Defence at the end of the
+        # game
+        elif self.highestContract == Game.contracts[4]:
+            self.guardAgainstCards = self._dog
+
+    def end(self) -> tuple:  # TODO: export data for following game
         # Returns all information that is needed to start a new game with the already existing deck and players
         pass
 
@@ -205,7 +239,7 @@ class Player:
         self.name = str(name)
 
         validStrategies = ['human', 'random']
-        if strategy in validStrategies:  # TODO: more strategies
+        if strategy in validStrategies:  # TODO: (LONG TERM) MORE STRATEGIES (AI)
             self.strategy = strategy
         else:
             raise PlayerException("Please use one of the following strategies:", validStrategies)
@@ -228,7 +262,7 @@ class Player:
 
         # Show the question to the user if the user is a human player and return the option chosen by the player
         if self.strategy == 'human':
-            print(f"{self.name} hand: {self.hand}:")
+            print(f"{self.name} hand: {sorted(self.hand, key=self._handSortKey)}:")
             print(question)
 
             playerAnswer = None
@@ -242,44 +276,44 @@ class Player:
         elif self.strategy == 'random':
             return rd.choice(options)
 
-    def addCardsToHand(self, cards: list) -> None:
+    def addCardsToHand(self, cards: list, sort=False) -> None:
         self.hand.extend(cards)
-        self.hand.sort(key=self.handSortKey)
+        if sort:
+            self.hand.sort(key=self._handSortKey)
+
+    def _handSortKey(self, s: str) -> str:
+        return Game.create_deck().index(s)
 
     def clearHand(self) -> list:
-        oldHand, self.hand = self.hand, []
+        """Empties a player's hand and returns the cards that were in it"""
+        oldHand, self.hand = self.hand.copy(), []
         return oldHand
 
-    def putCardToAside(self) -> None:
+    def takeCardsWon(self, cards: list):
+        self.cardsWon.extend(cards)
+
+    def putCardInAside(self) -> None:
         """Ask the player to choose which cards we would like to transfer from his hand to the aside, keeping in mind
          the limitations when doing so"""
         # Define the question
         question = f"Choose a card in your hand to put into your aside."
 
         # Select the options
-        options = self.hand
+        options = self.hand.copy()
         # Kings cannot be put into the aside
-        for king in ['K' in option for option in options]:
+        for king in [option for option in options if 'K' in option]:
             options.remove(king)
         # If there are fewer cards that aren't trumps then the number of cards missing in the aside
-        if len(['T' in option for option in options]) < Game.dogSizes[self.game.playerCount] - self.cardsWon:
-            ...
+        missingInAsideCount = Game.dogSizes[self.game.playerCount] - len(self.cardsWon)
+        if len([option for option in options if 'T' not in option]) >= missingInAsideCount:
+            for trump in [option for option in options if 'T' in option]:
+                options.remove(trump)
+        # The excuse cannot be put into the aside
+        if 'EX' in options:
+            options.remove('EX')
 
         card = self._getDecision(question, options)
         self.cardsWon.append(self.hand.pop(self.hand.index(card)))
-
-    def handSortKey(self, s: str) -> str:  # TODO: fix letters before numbers in suits
-        if 'T' not in s and 'EX' not in s:
-            # To get all the simple cards to the beginning of the list
-            s = s[::-1]
-            # And the face cards to the end of this sub-list
-            if not s.endswith(('J', 'C', 'Q', 'K')):
-                s = s
-        if s == 'EX':
-            pass
-        else:
-            s = '0' + s[:-1]
-        return s
 
     def chooseContract(self, highestContract='pass'):
         highestContractIndex = Game.contracts.index(highestContract)
@@ -300,11 +334,20 @@ class PlayerException(Exception):
 
 
 if __name__ == '__main__':
-    game = Game(4)
-    game.start()
+    game = Game(3)
+    game.play()
     print()
-    game2 = Game(5, lastDealer=3)
-    game2.start()
+    game2 = Game(4, lastDealer=3)
+    game2.play()
     print()
-    game3 = Game(3)
-    game3.start()
+    game3 = Game(5)
+    game3.play()
+
+    exit()
+    games = 0  # NOQA
+    while n < 1000000:
+        start = time.perf_counter_ns()
+        game = Game(rd.randint(3, 5), lastDealer=rd.randint(0, 3))
+        total = time.perf_counter_ns() - start
+        print(total, games)
+        games += 1
