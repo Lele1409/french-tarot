660612e376430a51677f5db793596c1b08c9f30c
Lele1409 <leander.decoene@gmail.com>
2023-07-02 23:09:41 +0200

    modify test.py

--- tests/test1.py
+++ tests/test1.py
@@ -16,17 +16,13 @@ if __name__ == '__main__':
     # Set execution mode to random
     src.game.MODE = 'random'
 
-    GAMES = 0  # Count of already performed games
-    TESTS = 1e6  # Total number of tests to run
-    TOTALSTART = time.perf_counter_ns()  # Performance timer start
-    while GAMES < TESTS:
+    while True:
         start = time.perf_counter_ns()  # Single test performance timer start
 
         # Try running the game if it fails at some point show debug information
         game = src.game.Game(rd.randint(3, 5), lastDealer=rd.randint(0, 3))
         try:
             game.play()
-            src.utils.printAllGameFields(game)
         except Exception as e:
             src.utils.printAllGameFields(game)
             raise e
@@ -36,7 +32,3 @@ if __name__ == '__main__':
 
         # Run tests
         testsAfterPlaying(game)
-
-        GAMES += 1
-    TOTALTIME = time.perf_counter_ns() - TOTALSTART
-    print('total:', TOTALTIME, 'average:', TOTALTIME / TESTS)
