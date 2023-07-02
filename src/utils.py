0eca8ad293726b54b5a77a681a6d8928fe771f28
Lele1409 <leander.decoene@gmail.com>
2023-07-02 18:07:08 +0200

    add a memoize function to utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,15 @@ def printAllGameFields(gameInstance):
             for player in field_value:
                 for field_name_, field_value_ in vars(player).items():
                     print('└──', field_name_.ljust(35), field_value_)
+
+
+class Memoize:
+    def __init__(self, f):
+        self.f = f
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.f(*args)
+        # Warning: You may wish to do a deepcopy here if returning objects
+        return self.memo[args]
