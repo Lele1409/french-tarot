8fdae230566068189738c8fac8e0c5c90e5888e1
Lele1409 <leander.decoene@gmail.com>
2023-08-23 19:25:22 +0200

    start dev

---
+++ src/game.py
@@ -0,0 +1,29 @@
+class Game:
+    def __init__(self):
+        self.deck: list[Card] = []
+        self.dog: list[Card] = []
+        self.aside: list[Card] = []
+        self.current_player: str = None
+        self.tricks: list[Trick] = []
+        self.contract_type: str = None
+        self.taking_player: str = None
+        self.calling_player: str = None
+        self.chelem_player: str = None
+        self.handful_players: list[Player] = []
+
+    def dealCards(self, deck, players):
+        ...
+    def chooseContract(self, current_player):
+        #contract
+        self.contract_type=
+        self.taking_player=
+    def createDog(self, deck):
+        self.dog=deck
+
+    def
+
+
+ObjectNames
+functionNames
+variable_names
+__private_variable
