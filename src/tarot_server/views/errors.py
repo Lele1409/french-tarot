24eb4ce4aa43eabe44db2705a547789fc43b30d9
Lele1409 <leander.decoene@gmail.com>
2024-01-18 16:37:42 +0100

    add placeholder to latest form and fix typo for 501 code

--- src/tarot_server/views/errors.py
+++ src/tarot_server/views/errors.py
@@ -4,16 +4,16 @@ views_errors = Blueprint('errors', 'tarot_server')
 
 
 @views_errors.app_errorhandler(403)
-def error_403_forbidden(e):
+def error_403_forbidden():
 	return render_template('errors/403.html'), 403
 
 
 @views_errors.app_errorhandler(404)
-def error_404_page_not_found(e):
+def error_404_page_not_found():
 	return render_template('errors/404.html'), 404
 
 
 @views_errors.route('/not-implemented')
 @views_errors.app_errorhandler(501)
 def error_501_not_implemented():
-	return render_template('errors/not_implemented.html'), 501
+	return render_template('errors/501.html'), 501
