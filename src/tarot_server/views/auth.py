0b8490b55f4875439917fc1c5ed78f08695061b0
Lele1409 <leander.decoene@gmail.com>
2024-03-15 19:42:02 +0100

    reformat unused code

--- src/tarot_server/views/auth.py
+++ src/tarot_server/views/auth.py
@@ -12,46 +12,46 @@ views_auth = Blueprint('auth', 'tarot_server')
 
 @views_auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-	if current_user.is_authenticated:
-		return redirect(url_for('menu.menu'))
+    if current_user.is_authenticated:
+        return redirect(url_for('menu.menu'))
 
-	signup_form = SignupForm()
+    signup_form = SignupForm()
 
-	if signup_form.is_submitted() and \
-			signup_form.agreements.validate(signup_form):
-		if 'anon_signup-submit_anon' in request.form and \
-				signup_form.anon_signup.validate(signup_form):
-			e, p = sign_up(None, None, anon=True)
-			log_in(e)
-			return redirect(url_for('menu.menu'))
+    if signup_form.is_submitted() and \
+            signup_form.agreements.validate(signup_form):
+        if 'anon_signup-submit_anon' in request.form and \
+                signup_form.anon_signup.validate(signup_form):
+            email = sign_up(None, None, anon=True)[0]
+            log_in(email)
+            return redirect(url_for('menu.menu'))
 
-		elif 'standard_signup-submit_standard' in request.form and \
-				signup_form.standard_signup.validate(signup_form):
-			sign_up(signup_form.standard_signup.email.data,
-					signup_form.standard_signup.password.data)
-			return redirect(url_for('auth.login'))
-	return render_template('auth/signup.html', form=signup_form)
+        elif 'standard_signup-submit_standard' in request.form and \
+                signup_form.standard_signup.validate(signup_form):
+            sign_up(signup_form.standard_signup.email.data,
+                    signup_form.standard_signup.password.data)
+            return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', form=signup_form)
 
 
 @views_auth.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('menu.menu'))
+    if current_user.is_authenticated:
+        return redirect(url_for('menu.menu'))
 
-	login_form = LoginForm()
-	if login_form.validate_on_submit():
-		# Login the user of which the login information has been verified
-		log_in(login_form.email.data)
-		return redirect(url_for('menu.menu'))
-	return render_template('auth/login.html', form=login_form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        # Login the user of which the login information has been verified
+        log_in(login_form.email.data)
+        return redirect(url_for('menu.menu'))
+    return render_template('auth/login.html', form=login_form)
 
 
 @views_auth.route('/logout')
 def logout():
-	"""Discards the user's session login, thus logging them out on the server
+    """Discards the user's session login, thus logging them out on the server
 	side and redirecting them to the login page."""
 
-	# Formal logout
-	log_out()
-	# Visual logout
-	return redirect(url_for('auth.login'))
+    # Formal logout
+    log_out()
+    # Visual logout
+    return redirect(url_for('auth.login'))
